ef1a4608cdb7125896bb7d0efd332690d4f0c2bd
Prih Vale <ps.vale@hotmail.com>
2017-11-15 16:29:26 -0200

    Ajuste da tela inicial
    Signed-off-by: Prih Vale <ps.vale@hotmail.com>

--- app/src/main/java/com/example/leandro/satisfyhunger/MainActivity.java
+++ app/src/main/java/com/example/leandro/satisfyhunger/MainActivity.java
@@ -1,27 +1,13 @@
 package com.example.leandro.satisfyhunger;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.UUID;
-
-import model.Vendedor;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_vendedor);
+        setContentView(R.layout.activity_main);
     }
 }
