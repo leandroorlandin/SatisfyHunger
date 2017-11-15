1d9d757282e03d7f2c136cbafc0b7d55db7423b2
Prih Vale <ps.vale@hotmail.com>
2017-11-15 11:26:15 -0200

    Adicionando tela de login
    Signed-off-by: Prih Vale <ps.vale@hotmail.com>

--- app/src/main/java/com/example/leandro/satisfyhunger/MainActivity.java
+++ app/src/main/java/com/example/leandro/satisfyhunger/MainActivity.java
@@ -19,76 +19,9 @@ import model.Vendedor;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editNome , editEmail, editLocal, editTelefone, editProduto, editDescricao;
-    ListView list_vDados;
-
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        editNome = (EditText)findViewById(R.id.editNome);
-        editEmail = (EditText)findViewById(R.id.editEmail);
-        editLocal = (EditText)findViewById(R.id.editLocal);
-        editTelefone = (EditText)findViewById(R.id.editTelefone);
-        editProduto = (EditText)findViewById(R.id.editProduto);
-        editDescricao  = (EditText)findViewById(R.id.editDescricao);
-        list_vDados = (ListView) findViewById(R.id.list_vDados);
-        inicializarFirebase();
-
-    }
-     private void inicializarFirebase(){
-         FirebaseApp.initializeApp(this);
-         firebaseDatabase = FirebaseDatabase.getInstance();
-         databaseReference = firebaseDatabase.getReference();
-     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.menu_novo){
-            Vendedor vendedor = new Vendedor();
-            vendedor.setId(UUID.randomUUID().toString());
-            vendedor.setNome(editNome.getText().toString());
-            vendedor.setEmail(editEmail.getText().toString());
-            vendedor.setLocal(editLocal.getText().toString());
-            vendedor.setTelefone(editTelefone.getText().toString());
-            vendedor.setProduto(editProduto.getText().toString());
-            vendedor.setDescricao(editDescricao.getText().toString());
-            vendedor.setTipo(1);
-            vendedor.setStatus(1);
-            databaseReference.child("Vendedor").child(vendedor.getId()).setValue(vendedor);
-            limparCampos();
-            
-        }else if(id == R.id.menu_altera){
-
-        }else if(id == R.id.menu_deleta) {
-
-
-        }else if(id == R.id.menu_buscar){
-            Intent i = new Intent(MainActivity.this,Pesquisa.class);
-            startActivity(i);
-        }
-
-        return true;
-    }
-
-    private void limparCampos() {
-        editNome.setText("");
-        editEmail.setText("");
-        editLocal.setText("");
-        editTelefone.setText("");
-        editProduto.setText("");
-        editDescricao.setText("");
+        setContentView(R.layout.activity_cadastro_vendedor);
     }
 }
